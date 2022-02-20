2315f1001af12405c10441bd22ca7c6bcd82a172
myofficialthing@gmail.com <myofficialthing@gmail.com>
2022-02-20 11:02:12 +0530

    driver updates

--- src/test/java/com/selenium/factory/DriverManagerClass.java
+++ src/test/java/com/selenium/factory/DriverManagerClass.java
@@ -9,14 +9,30 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverManagerClass {
 	
-	
-	public WebDriver initializDriver() {
-		WebDriverManager.chromedriver().setup();
-		//System.setProperty("webdriver.chrome.driver", "F:\\chromedriverexe\\chromedriver.exe");
-		//WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
-		//driver.get("https://askomdch.com");
-		driver.manage().window().maximize();
+	WebDriver driver;
+	public WebDriver initializDriver(String browser) {
+		
+		//String browserName=System.getProperty("browser",browser);
+		String browserName=browser;
+		switch(browserName) {
+		case "Chrome":
+			System.setProperty("webdriver.chrome.driver", "F:\\chromedriverexe\\chromedriver.exe");
+			driver=new ChromeDriver();
+			break;
+		case "Firefox":
+			System.out.println("firefox going to exceute");
+			break;
+			default: 
+				throw new IllegalStateException("invalid browser name: "+browserName);
+		}
+		
+		
+//		WebDriverManager.chromedriver().setup();
+//		//System.setProperty("webdriver.chrome.driver", "F:\\chromedriverexe\\chromedriver.exe");
+//		//WebDriverManager.chromedriver().setup();
+//		WebDriver driver=new ChromeDriver();
+//		//driver.get("https://askomdch.com");
+//		driver.manage().window().maximize();
 		return driver;
 	}
 
--- src/test/java/com/selenium/pom/BaseTest.java
+++ src/test/java/com/selenium/pom/BaseTest.java
@@ -5,17 +5,18 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import com.selenium.factory.DriverManagerClass;
 
 public class BaseTest {
 	protected WebDriver driver;
 	
-	
+	@Parameters("browser")
 	@BeforeMethod
-	public void driverStart()
+	public void driverStart(String browser)
 	{
-		driver=new DriverManagerClass().initializDriver();
+		driver=new DriverManagerClass().initializDriver( browser);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
 	}
 	
